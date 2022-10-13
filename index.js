cf45511c34bd1649ebd5acf8db03323c1936a719
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-13 09:09:39 +0800

    finalize error handling if 3rd arg was undefined

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const fs = require('fs')
 
 const argument = process.argv.slice(2)
-
-const fileReader = (arg, firstArgContent) => {
-  fs.readFile(arg, (nonExistent) => {
-    if (!nonExistent) {
-      console.log(`${arg}: File Already Exists`)
-    } else {
-      const fileNameGetter = argument[1].toString().split('/')
-      fs.writeFileSync(fileNameGetter[1], firstArgContent.join('\n'))
-      console.log(`${fileNameGetter[1]} created`)
-      switch (argument[2]) {
-        case '-n':
-        case undefined:
-          break
-        case '-y':
-          fs.writeFileSync(fileNameGetter[1], firstArgContent.join('\n'))
-          console.log(`File ${fileNameGetter[1]} was overwritten`)
-          break
-        default:
-          console.log(`ERROR: Invalid ${argument[2]} Argument`)
-      }
-    }
-  })
+const fileReader = (firstArgContent) => {
+  const fileNameGetter = argument[1].toString()
+  switch (argument[2]) {
+    case '-n':
+    case undefined:
+      fs.readFile(argument[1], (nonExistent) => {
+        if (!nonExistent) {
+          console.log(`${argument[1]} already exists`)
+        } else {
+          fs.writeFileSync(fileNameGetter, firstArgContent.join('\n'))
+          console.log(`${fileNameGetter} created`)
+        }
+      })
+      break
+    case '-y':
+      fs.readFile(argument[1], (nonExistent) => {
+        if (nonExistent) {
+          console.log(`${argument[1]} exists`)
+        } else {
+          fs.writeFileSync(fileNameGetter, firstArgContent.join('\n'))
+          console.log(`${fileNameGetter} overwritten`)
+        }
+      })
+      break
+    default:
+      console.log(`ERROR: Invalid ${argument[2]} Argument`)
+  }
 }
 
 const data = fs.readFileSync(argument[0], { encoding: 'utf-8', flag: 'r' }).toString().split('\n')
@@ -33,10 +38,10 @@ for (let j = 0; j < data.length; j += 1) {
 
 if (argument.length < 2) {
   console.log('Missing 2nd Argument (Required)')
+} else if (argument.length === 2) {
+  fileReader(firstArgContent)
 } else {
-  fileReader(argument[0], firstArgContent)
-}
-
-for (let i = 1; i <= argument.length - 1; i += 1) {
-  fileReader(argument[i], firstArgContent)
+  for (let i = 1; i < argument.length - 1; i += 1) {
+    fileReader(firstArgContent)
+  }
 }
