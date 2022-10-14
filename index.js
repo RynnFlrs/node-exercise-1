0d04bb402c4a5ca4cc3480327615b7c85b43572e
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-14 11:17:23 +0800

    Update changes on the implementation of switch

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 const fs = require('fs')
 
 const argument = process.argv.slice(2)
-const fileReader = (firstArgContent) => {
-  const fileNameGetter = argument[1].toString()
-  switch (argument[2]) {
+const [contentFile, outputFile, flag] = [...argument]
+
+if (argument.length < 2) {
+  console.log('Missing 2nd Argument (Required)')
+  process.exit(1)
+} else {
+  const data = fs.readFileSync(contentFile, { encoding: 'utf-8', flag: 'r' }).toString().split('\n')
+  const contentFileContent = []
+  for (let j = 0; j < data.length; j += 1) {
+    contentFileContent[j] = `${j} : ${data[j]}`
+  }
+  const fileNameGetter = outputFile.toString()
+
+  switch (flag) {
     case '-n':
     case undefined:
-      fs.readFile(argument[1], (nonExistent) => {
+      fs.readFile(outputFile, (nonExistent) => {
         if (!nonExistent) {
-          console.log(`${argument[1]} already exists`)
+          console.log(`${outputFile} already exists`)
+          process.exit(1)
         } else {
-          fs.writeFileSync(fileNameGetter, firstArgContent.join('\n'))
-          console.log(`${fileNameGetter} created`)
+          fs.writeFileSync(fileNameGetter, contentFileContent.join('\n'))
+          console.log(`${fileNameGetter} doesnt exists and is created`)
         }
       })
       break
     case '-y':
-      fs.readFile(argument[1], (nonExistent) => {
-        if (nonExistent) {
-          console.log(`${argument[1]} exists`)
+      fs.readFile(outputFile, (nonExistent) => {
+        if (!nonExistent) {
+          fs.writeFileSync(fileNameGetter, contentFileContent.join('\n'))
+          console.log(`${fileNameGetter} exists and is overwritten`)
+          process.exit(1)
         } else {
-          fs.writeFileSync(fileNameGetter, firstArgContent.join('\n'))
-          console.log(`${fileNameGetter} overwritten`)
+          fs.writeFileSync(fileNameGetter, contentFileContent.join('\n'))
+          console.log(`${fileNameGetter} doesnt exists and is created`)
         }
       })
       break
     default:
-      console.log(`ERROR: Invalid ${argument[2]} Argument`)
-  }
-}
-
-const data = fs.readFileSync(argument[0], { encoding: 'utf-8', flag: 'r' }).toString().split('\n')
-const firstArgContent = []
-for (let j = 0; j < data.length; j += 1) {
-  firstArgContent[j] = `${j} : ${data[j]}`
-}
-
-if (argument.length < 2) {
-  console.log('Missing 2nd Argument (Required)')
-} else if (argument.length === 2) {
-  fileReader(firstArgContent)
-} else {
-  for (let i = 1; i < argument.length - 1; i += 1) {
-    fileReader(firstArgContent)
+      console.log(`ERROR: Invalid ${flag} Argument`)
+      process.exit(1)
   }
 }
